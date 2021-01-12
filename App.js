bb90b8f6514255df894c1c589c37899de16554b1
BrandonMarchena <brandon.marchena@aplinet.co>
2021-01-12 00:37:51 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View>
+        <Text>BIRTHDAY APP</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
